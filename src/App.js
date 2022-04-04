019a8a811d4bbc4ec3a8dcad97a281e83d1497d9
MD Rony Mia <official.mdronymia@gmail.com>
2022-04-04 14:51:03 +0600

    navBar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import Header from './Components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Review from './Components/Review/Review';
+import DashBoard from './Components/DashBoard/DashBoard';
+import Blogs from './Components/Blogs/Blogs';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/review' element={<Review></Review>}></Route>
+        <Route path='/dashboard' element={<DashBoard></DashBoard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <nav className='font-semibold h-12 bg-gray-200 flex justify-center items-center'>
+            <Link to='/home'>Home</Link>
+            <Link to='/review'>Review</Link>
+            <Link to='/dashboard'>Dashboard</Link>
+            <Link to='/blogs'>Blogs</Link>
+            <Link to='/about'>About</Link>
+        </nav>
     );
 };
 
